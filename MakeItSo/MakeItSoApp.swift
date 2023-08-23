ab418a0d2277dc84f3c4942eb5ba198808658939
Leonardo Caracho <leeo.apc@gmail.com>
2023-08-23 19:18:34 -0300

    Initial Commit

---
+++ MakeItSo/MakeItSoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeItSoApp.swift
+//  MakeItSo
+//
+//  Created by Leonardo Caracho on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MakeItSoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
